c401b39c8bf6ec5f560417afc50d92ef8e5b19bf
jingxian99 <2439644676@qq.com>
2020-08-04 23:16:21 +0800

    0804晚上小练习测试

--- src/main/java/dell/Day01_0804/Night/Test03.java
+++ src/main/java/dell/Day01_0804/Night/Test03.java
@@ -5,21 +5,21 @@ import java.util.Scanner;
 public class Test03 {
     public static void main(String[] args) {
         double b = 0.0001f;
-        System.out.println("请输入你的月薪:");
+        System.out.print("请输入你的月薪:");
         Scanner sc = new Scanner(System.in);
         double a = sc.nextDouble();
         if (a <= 10){
             b = a*1.1;
         }else if (a <= 20) {
-            b = a * 1.1 + (a-10)*1.075;
+            b = 10 * 1.1 + (a-10)*1.075;
         }else if (a <= 40){
-            b = a * 1.1+(a-10)*1.075 + (a-20)*1.05;
+            b = 10 * 1.1+(a-10)*1.075 + (a-20)*1.05;
         }else if (a <= 60) {
-            b = a * 1.1+(a-10)*1.075 + (a-20)*1.05 + (a-40)*1.03;
+            b = 10 * 1.1+(a-10)*1.075 + (a-20)*1.05 + (a-40)*1.03;
         }else if(a <= 100){
-            b = a * 1.1+(a-10)*1.075 + (a-20)*1.05 + (a-40)*1.03 + (a-60)*1.015;
+            b = 10 * 1.1+(a-10)*1.075 + (a-20)*1.05 + (a-40)*1.03 + (a-60)*1.015;
         }else{
-            b = a * 1.1+(a-10)*1.075 + (a-20)*1.05 + (a-40)*1.03 + (a-60)*1.015 + (a-100)*1.01;
+            b = 10 * 1.1+(a-10)*1.075 + (a-20)*1.05 + (a-40)*1.03 + (a-60)*1.015 + (a-100)*1.01;
         }
         System.out.println("应该发放的奖金金额为:"+b+"万元");
 
