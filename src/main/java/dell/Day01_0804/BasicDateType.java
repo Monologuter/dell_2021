9372948e6097fb1a50dc45deef19f281531ea991
jingxian99 <2439644676@qq.com>
2020-08-04 09:12:23 +0800

    Initial commit

---
+++ src/main/java/dell/Day01_0804/BasicDateType.java
@@ -0,0 +1,10 @@
+package dell.Day01_0804;
+
+public class BasicDateType {
+    //单行注释
+    public static void main(String[] args) {
+        System.out.println("张三");
+        //你是猪吗  单行注释
+
+    }
+}
