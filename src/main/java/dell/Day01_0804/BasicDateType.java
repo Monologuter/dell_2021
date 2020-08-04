86abef446f3bdd168a83ba91af2e6cc5fa1d537f
jingxian99 <2439644676@qq.com>
2020-08-04 10:40:51 +0800

    命名规范

--- src/main/java/dell/Day01_0804/BasicDateType.java
+++ src/main/java/dell/Day01_0804/BasicDateType.java
@@ -6,5 +6,14 @@ public class BasicDateType {
         System.out.println("张三");
         //你是猪吗  单行注释
 
+        //年龄
+        System.out.println(23);
+
+        //是否结婚
+        System.out.println(false);
+
+        //身高
+        System.out.println(1.777);
+
     }
 }
