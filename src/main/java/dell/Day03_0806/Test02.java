2cf3b45e52feecc2bf660986a83921826ae1f096
jingxian99 <2439644676@qq.com>
2020-08-06 14:58:38 +0800

    数组练习

---
+++ src/main/java/dell/Day03_0806/Test02.java
@@ -0,0 +1,18 @@
+package dell.Day03_0806;
+
+/**
+ * @Author 马小姐
+ * @Date 2020/8/6 14:01
+ * @Version 1.0
+ * @Description:
+ */
+public class Test02 {
+    public static void main(String[] args) {
+        double sum = 0.0;
+        double Array[] = new double[] {12.6,77.0,45.7,2222,5};
+        for (int i = 0; i <=Array.length-1 ; i++) {
+            sum += Array[i];
+        }
+        System.out.println("数组的和为"+sum);
+    }
+}
