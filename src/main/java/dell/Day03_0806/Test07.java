5278b75be22485a6fb71328a08e1c723aec93af8
jingxian99 <2439644676@qq.com>
2020-08-07 09:42:19 +0800

    0806总结

--- src/main/java/dell/Day03_0806/Test07.java
+++ src/main/java/dell/Day03_0806/Test07.java
@@ -1,6 +1,7 @@
 package dell.Day03_0806;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * @Author 马小姐
@@ -14,12 +15,14 @@ import java.util.*;
 * */
 public class Test07 {
     public static void main(String[] args) {
-        System.out.println("请输入数组的长度：");
+        System.out.println("请输入初始化数组元素的个数：");
         Scanner sc = new Scanner(System.in);
 
         //num表示初始的时候数组的长度
         int num = sc.nextInt();
-        int[] Array = new int[num];
+        int[] Array = new int[num+1];
+
+        System.out.println("数组长度为："+Array.length);
 
 
         System.out.print("请输入"+num+"个数：");
@@ -27,6 +30,7 @@ public class Test07 {
             Array[i] = sc.nextInt();
         }
 
+        //数组排序
         Arrays.sort(Array);//Arrays是util包
         for(int j : Array){
         }
@@ -43,40 +47,10 @@ public class Test07 {
             }
         }
 
-        System.out.println("\n");
-        System.out.println("请输入你想要插入的数字个数：");
-        int numSecond = sc.nextInt();
-        System.out.print("我想要插入"+numSecond+"个数：");
 
 
-
-        for (int i = 0; i <Array.length+numSecond ; i++) {
-            Array[i] = sc.nextInt();
-        }
-
-        Arrays.sort(Array);//Arrays是util包
-        for(int j : Array){
-        }
-
-        /*
-        * 下面插入操作没有做出来 数组一旦创建数组的长度就不可改变
-        * 应该使用集合做插入数据操作
-        *
-        *
-        *
-        * */
-
-        for (int n = 0;  n<=Array.length-1+numSecond ; n++) {
-            if (n==0){
-                System.out.print("打印出来的数组为："+"["+Array[0]+",");
-            }else{
-                if(n==Array.length-1+numSecond){
-                    System.out.print(Array[n]+"]");
-                }else if(n<Array.length-1+numSecond){
-                    System.out.print(Array[n]+",");
-                }
-            }
-        }
+        //克隆数组  clone(); 方法
+        int  ArraySecond [] = Array.clone();
 
 
     }
